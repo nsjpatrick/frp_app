d3e15fbf8cbd9f8b92a5a98b9b10fb5ae5d502a1
Nate Patrick <nsjpatrick@me.com>
2026-04-20 17:24:34 -0400

    refactor: retire wall-thickness stub in favor of real computeWallThickness

--- lib/rules/index.ts
+++ lib/rules/index.ts
@@ -1,5 +1,7 @@
 export * from './certification-filter';
 export * from './compatibility';
-export * from './wall-thickness-stub';
+export * from './wall-thickness';
+export * from './constants';
+export * from './types';
 
-export const RULES_ENGINE_VERSION = '0.1.0-walking-skeleton';
+export const RULES_ENGINE_VERSION = '0.2.0-rules-depth';
